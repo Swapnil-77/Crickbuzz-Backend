6333d106fcd2d046fff7e0838e4acac11f918e94
Swapnil-77 <swapnilphad07@gmail.com>
2026-06-10 21:57:24 +0530

    Update MatchController.java

--- src/main/java/com/swapnil/Crickbuzz/Backend/controller/MatchController.java
+++ src/main/java/com/swapnil/Crickbuzz/Backend/controller/MatchController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/matches")
+@RequestMapping("/match")
 @CrossOrigin
 public class MatchController {
 
