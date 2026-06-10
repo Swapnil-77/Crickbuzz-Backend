a939a6b82b0fa7f04f3e154b128ec5d70f944083
Swapnil-77 <swapnilphad07@gmail.com>
2026-06-10 22:01:00 +0530

    Change request mapping from '/match' to '/matches'

--- src/main/java/com/swapnil/Crickbuzz/Backend/controller/MatchController.java
+++ src/main/java/com/swapnil/Crickbuzz/Backend/controller/MatchController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/match")
+@RequestMapping("/matches")
 @CrossOrigin
 public class MatchController {
 
